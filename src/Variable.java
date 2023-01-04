3d854031e55bcd34e06201a5b5c3a477e3f3fcf3
Sanjida <sanjidasn@gmail.com>
2023-01-05 03:36:11 +0900

    WELCOME sELENIUM Test

---
+++ src/Variable.java
@@ -0,0 +1,13 @@
+public class Variable {
+    protected String lastName;
+    static public double amount;
+
+    public void printSomething() {
+        String someThings = "Some Things";
+        System.out.println(someThings);
+    }
+
+    public void doSomeThings() {
+        System.out.println("Static : "+amount);
+    }
+}
---
+++ src/VariableTest.java
@@ -0,0 +1,8 @@
+public class VariableTest {
+    public static void main(String[] args) {
+        Variable.amount = 10;
+        Variable variable = new Variable();
+        variable.lastName = "SANJIDA";
+        variable.printSomething();
+    }
+}
